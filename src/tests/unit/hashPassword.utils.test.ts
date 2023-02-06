aefb4b21d3c854058eac9c09dc12f764ce425d57
Nguyen Minh Tuan <tnowad@gmail.com>
2023-02-06 14:12:48 +0700

    add unit test for hashPassword

--- src/tests/unit/hashPassword.utils.test.ts
+++ src/tests/unit/hashPassword.utils.test.ts
@@ -22,3 +22,30 @@ describe('comparePasswordSync', () => {
 		expect(isSame).toBe(false)
 	})
 })
+
+describe('comparePassword', () => {
+	it('callback true if the passwords match', () => {
+		const plainPassword = 'password123'
+		const hashedPassword = hashPassword(plainPassword)
+		comparePassword(
+			plainPassword,
+			hashedPassword,
+			(err: Error | undefined, same: boolean) => {
+				expect(err).toBeNull()
+				expect(same).toBe(true)
+			},
+		)
+	})
+	it('callback false if the passwords do not match', () => {
+		const plainPassword = 'password123'
+		const hashedPassword = hashPassword(plainPassword)
+		comparePassword(
+			'wrongPassword',
+			hashedPassword,
+			(err: Error | undefined, same: boolean) => {
+				expect(err).toBeFalsy()
+				expect(same).toBe(false)
+			},
+		)
+	})
+})
